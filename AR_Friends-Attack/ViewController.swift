a046e853a59a13cc865a7f114438e78554e56a97
Jason Whittaker <jason_bournemouth@yahoo.co.uk>
2018-06-24 11:24:18 +0700

    Got the basics done

--- AR_Friends-Attack/ViewController.swift
+++ AR_Friends-Attack/ViewController.swift
@@ -23,6 +23,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var peopleAdded = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -73,11 +75,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBAction func addTargets(_ sender: Any) {
         
         // Add the appropriate amount of targets
-        let objectsToAdd = GameStateManager.sharedInstance().initialTargets
+      //  let objectsToAdd = GameStateManager.sharedInstance().initialTargets
         
-        for i in 1...objectsToAdd {
-            addFriends(numOfFriend: i)
-        }
+//        for i in 1...objectsToAdd {
+//            print("1. Add Friend")
+//            addFriends(numOfFriend: i)
+//        }
+        
+        peopleAdded += 1
+
+        addFriends(numOfFriend: peopleAdded)
+        
     
     }
     
@@ -89,12 +97,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
         case 1:
             target.name = "Harsh"
+            print("1.1 Add Friend")
+
         case 2:
             target.name = "Ploy"
+            print("1.2 Add Friend")
+
         case 3:
             target.name = "Scotto"
+            print("1.3 Add Friend")
+
         default:
             target.name = "Harsh"
+            print("1.4 Add Friend")
+
         }
         
         addNodeToScene(nodeFriend: target.name)
@@ -103,30 +119,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func addNodeToScene(nodeFriend: String) {
         
+        
         // Create a new scene and set it's position
         let targetScene = SCNScene(named: "target.scnassets/target.scn")!
         let targetNode = targetScene.rootNode.childNode(withName: "target", recursively: false)
         
         // Position of node
-        let x = randomNumbers(numA: -0.3, numB: 0.3)
-        let y = randomNumbers(numA: -0.3, numB: 0.3)
-        let z = randomNumbers(numA: -0.3, numB: 0.3)
+        let x = randomNumbers(numA: -0.5, numB: 0.5)
+        let y = randomNumbers(numA: -0.5, numB: 0.5)
+        let z = randomNumbers(numA: -0.5, numB: 0.5)
         targetNode?.position = SCNVector3(x,y,z)
+       // targetNode?.position = SCNVector3(0,0,-1)
         
         // Add images
         self.addFace(nodeName: "faceFront", targetNode: targetNode!, imageName: nodeFriend)
         self.addFace(nodeName: "faceBack", targetNode: targetNode!, imageName: nodeFriend)
-        //self.addWalls(nodeName: "sideDoorA", portalNode: portalNode, imageName: "b-frontA")
+        self.addLabel(nodeName: "nameLabelLeft", targetNode: targetNode!, imageName: nodeFriend)
+        self.addLabel(nodeName: "nameLabelRight", targetNode: targetNode!, imageName: nodeFriend)
+
         // self.addWalls(nodeName: "sideDoorB", portalNode: portalNode, imageName: "b-frontb")
         
         self.sceneView.scene.rootNode.addChildNode(targetNode!)
 
         
         // Rotate object
-        let nodeRotateAction = rotation(time: 8)
+        let nodeRotateAction = rotation(time: 15)
         targetNode?.runAction(nodeRotateAction)
-        
-
         
         animateNode(node: targetNode!)
         
@@ -146,10 +164,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func animateNode(node: SCNNode) {
         
+        let waitStart = SCNAction.wait(duration: 6.0)
         let moveDown = SCNAction.move(by: SCNVector3(0, -0.1, 0), duration: 1)
         let moveUp = SCNAction.move(by: SCNVector3(0,0.1,0), duration: 1)
         let waitAction = SCNAction.wait(duration: 0.25)
-        let hoverSequence = SCNAction.sequence([moveUp,waitAction,moveDown])
+        let hoverSequence = SCNAction.sequence([waitStart, moveUp,waitAction,moveDown])
         let loopSequence = SCNAction.repeatForever(hoverSequence)
         node.runAction(loopSequence)
         
@@ -164,19 +183,62 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     
     func addFace(nodeName: String, targetNode: SCNNode, imageName: String) {
+        
+        print("Node Check - \(nodeName), \(targetNode), \(imageName)")
+
         let child = targetNode.childNode(withName: nodeName, recursively: true)
-        child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Assets.xcassets/\(imageName).png")
-//        child?.renderingOrder = 200
+        child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "target.scnassets/\(imageName).png")
+        child?.renderingOrder = 200
 //        if let mask = child?.childNode(withName: "mask", recursively: false) {
 //            mask.geometry?.firstMaterial?.transparency = 0.000001
 //        }
     }
     
     func addLabel(nodeName: String, targetNode: SCNNode, imageName: String) {
-        let child = targetNode.childNode(withName: nodeName, recursively: true)
-        child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Assets.xcassets/\(imageName).png")
-        //child?.renderingOrder = 200
         
+        print("Label Check - \(nodeName), \(targetNode), \(imageName)")
+        
+        let child = targetNode.childNode(withName: nodeName, recursively: true)
+        
+        let labelScene = addLabel(text: imageName)
+        
+        print("Label Test2")
+        
+//        let material = SCNMaterial()
+//        material.isDoubleSided = true
+        // material.diffuse.contents = labelScene
+
+        print("Label Test3")
+
+        child?.geometry?.firstMaterial?.diffuse.contents = labelScene
+        child?.geometry?.firstMaterial?.isDoubleSided = true
+        
+        child?.renderingOrder = 200
+
+        
+        // child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "target.scnassets/\(imageName).png")
+        
+        
+        
+    }
+    
+    func addLabel(text: String) -> SKScene {
+        let skScene = SKScene(size: CGSize(width: 200, height: 200))
+        skScene.backgroundColor = UIColor.clear
+        
+        let rectangle = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 400, height: 400), cornerRadius: 10)
+        rectangle.fillColor = #colorLiteral(red: 0.6235300899, green: 0.8764483929, blue: 1, alpha: 1)
+        rectangle.strokeColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        rectangle.lineWidth = 5
+        rectangle.alpha = 0.4
+        let labelNode = SKLabelNode(text: text)
+        labelNode.fontSize = 46
+        labelNode.fontName = "SanFranciscoText-Bold"
+        labelNode.position = CGPoint(x:100,y:100)
+        skScene.addChild(rectangle)
+        skScene.addChild(labelNode)
+        
+        return skScene
     }
     
     // MARK: - ARSCNViewDelegate
