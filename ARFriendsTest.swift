4199fddde3d9d817507dc606aca273c6c7705215
Jason Whittaker <jason_bournemouth@yahoo.co.uk>
2019-09-24 22:13:50 +0700

    Setting up Tests - Before Renaming Project

---
+++ ARFriendsTest.swift
@@ -0,0 +1,41 @@
+//
+//  ARFriendsTest.swift
+//  AR_Friends-Attack
+//
+//  Created by JasonMac on 24/9/2562 BE.
+//  Copyright © 2562 JasonMac. All rights reserved.
+//
+
+import XCTest
+@testable import AR_Friends
+
+class ARFriendsTest: XCTestCase {
+    
+    var gc: GameController!
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testConversionForOne() {
+        let result =
+        XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+    }
+
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+
+}
---
+++ AR_Friends-AttackTests/AR_Friends_AttackTests.swift
@@ -0,0 +1,38 @@
+//
+//  AR_Friends_AttackTests.swift
+//  AR_Friends-AttackTests
+//
+//  Created by JasonMac on 24/9/2562 BE.
+//  Copyright © 2562 JasonMac. All rights reserved.
+//
+
+import XCTest
+//@testable import AR_Friends-Attack
+
+
+class AR_Friends_AttackTests: XCTestCase {
+    
+//    var sut: GameViewController!
+
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
